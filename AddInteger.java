f9ee837e258f1bafb4e5fb0b40fd0a0d9aef7ddb
ArunPandyanVasam <you@example.com>
2023-06-08 19:19:49 -0400

    version1

---
+++ AddInteger.java
@@ -0,0 +1,10 @@
+package IntAddition;
+
+public class AddInteger {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 1;
+
+        System.out.println("x+y: " + (x + y));
+    }
+}
